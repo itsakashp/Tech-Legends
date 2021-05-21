fc99bedb6ca8791f0f82762ae9ffb8caa982ae44
Akash <57492019+itsakashp@users.noreply.github.com>
2021-05-21 16:34:18 +0530

    Fixing some Common Security Issues
    avoiding cross-site scripting(XSS), Mongo Injection, and setting HTTP headers using helmet.js

--- schemas.js
+++ schemas.js
@@ -1,10 +1,32 @@
-const Joi = require('joi');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
 
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
 module.exports.legendSchema = Joi.object({
-    name:Joi.string().required().min(4).max(30),
+    name:Joi.string().required().min(4).max(30).escapeHTML(),
     DOB: Joi.date().required(),
-    POB:Joi.string().required(),
-    about:Joi.string().required(),
+    POB:Joi.string().required().escapeHTML(),
+    about:Joi.string().required().escapeHTML(),
     deleteImages:Joi.array(),
 });
 
